930adf7f8dd55c3e4a6ecc30ef7aa7f6b08059a2
Franky <56810823+Franky-Lim24@users.noreply.github.com>
2023-06-30 21:03:02 +0800

    Update RecipeService.java

--- src/main/java/com/chatgptgoestoschool/recipediscovery/service/RecipeService.java
+++ src/main/java/com/chatgptgoestoschool/recipediscovery/service/RecipeService.java
@@ -6,13 +6,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.chatgptgoestoschool.recipediscovery.model.Recipe;
 import com.chatgptgoestoschool.recipediscovery.repository.RecipeRepository;
+import com.chatgptgoestoschool.recipediscovery.utils.Spoonacular;
 
 @Service
 public class RecipeService {
   @Autowired
   private RecipeRepository recipeRepository;
 
+  @Autowired
+  private Spoonacular spoonacular;
+
+
   public List<Recipe> searchRecipe(String keyword) {
+    try {
+      List<Recipe> recipes = spoonacular.searchRecipes(keyword);
+      recipeRepository.saveAll(recipes);
+    } catch (Exception ex) {
+      System.out.println("An error occured " + ex.getMessage());
+    }
     return recipeRepository.searchRecipes(keyword);
   }
 }
