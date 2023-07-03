f8e78d08726b9a046ab4c539c8ae127888af328f
Franky <56810823+Franky-Lim24@users.noreply.github.com>
2023-07-03 17:49:46 +0800

    add jwt decoder

---
+++ src/main/java/com/chatgptgoestoschool/recipediscovery/utils/JWT.java
@@ -0,0 +1,20 @@
+package com.chatgptgoestoschool.recipediscovery.utils;
+
+import java.security.Key;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+
+@Component
+public class JWT {
+  @Value("${JWT_SECRET}")
+  private String secret;
+
+  public Jws<Claims> decodeToken(String token) {
+    Key key = Keys.hmacShaKeyFor(secret.getBytes());
+    return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+  }
+}
